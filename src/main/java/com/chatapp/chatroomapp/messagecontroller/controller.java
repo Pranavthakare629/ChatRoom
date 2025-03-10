3b68c319c0ef205396c7931e1e9ca2a60c763dc0
pranavthakare2024 <pranav.thakare@deptagency.com>
2025-03-10 17:27:41 +0530

    first commit

---
+++ src/main/java/com/chatapp/chatroomapp/messagecontroller/controller.java
@@ -0,0 +1,18 @@
+package com.chatapp.chatroomapp.messagecontroller;
+
+
+import com.chatapp.chatroomapp.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @MessageMapping("/message")
+    @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message) {
+        return message;
+
+    }
+}
